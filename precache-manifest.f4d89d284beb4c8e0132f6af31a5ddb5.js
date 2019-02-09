6a855beb8c84b2b7a932f7d1a6da28a2d99929db
reut <reut1990@gmail.com>
2019-02-09 21:11:03 +0200

    Updates

---
+++ precache-manifest.f4d89d284beb4c8e0132f6af31a5ddb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03abba1dc1a0ec6d0fa3",
+    "url": "/merkury2/static/js/runtime~main.03abba1d.js"
+  },
+  {
+    "revision": "01932937a41591e8df0a",
+    "url": "/merkury2/static/js/main.01932937.chunk.js"
+  },
+  {
+    "revision": "14a36fab30cc79210ca3",
+    "url": "/merkury2/static/js/1.14a36fab.chunk.js"
+  },
+  {
+    "revision": "01932937a41591e8df0a",
+    "url": "/merkury2/static/css/main.1a89fb16.chunk.css"
+  },
+  {
+    "revision": "1759d5ab1046aeb6c63789ba516faca7",
+    "url": "/merkury2/index.html"
+  }
+];
